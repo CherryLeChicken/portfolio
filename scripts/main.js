1d35268e02b05d22306818fcb6e04c655c1006f9
Cherry <cherryke07@gmail.com>
2025-03-20 16:22:26 -0400

    added files

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+let slideIndex = 0;
+
+function showSlides() {
+    let slides = document.querySelectorAll(".slide");
+    slides.forEach((slide, index) => {
+        slide.style.opacity = "0";
+        if (index === slideIndex) {
+            slide.style.opacity = "1";
+        }
+    });
+    slideIndex = (slideIndex + 1) % slides.length;
+    setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    showSlides();
+});
