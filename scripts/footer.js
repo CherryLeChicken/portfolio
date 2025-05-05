f52e781f80fbda67f44df395934bbbdebb8091e1
Cherry <cherryke07@gmail.com>
2025-05-05 16:48:23 -0400

    begym content & other content changes

--- scripts/footer.js
+++ scripts/footer.js
@@ -2,7 +2,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const footerContainer = document.getElementById('footer-container');
     if (footerContainer) {
-        fetch('footer.html')
+        fetch('./footer.html')
             .then(response => response.text())
             .then(data => footerContainer.innerHTML = data);
     }
